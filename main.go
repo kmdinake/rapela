4f9f04e851088130a659dbff7158a8145d5d202e
Dinake, Keoagile <kdinake@deloitte.co.za>
2026-04-22 18:21:17 +0200

    added bible verse type

--- main.go
+++ main.go
@@ -2,6 +2,31 @@ package main
 
 import "fmt"
 
+type BibleVerse struct {
+	Id      int    `json:"id"`
+	Book    string `json:"book"`
+	Chapter int    `json:"chapter"`
+	Verse   int    `json:"verse"`
+	Text    string `json:"text"`
+}
+
+func (v BibleVerse) String() string {
+	return fmt.Sprintf("Verse: %d, %s %d:%d - %s", v.Id, v.Book, v.Chapter, v.Verse, v.Text)
+}
+
+func getVerse() BibleVerse {
+	return BibleVerse{
+		Id:      1,
+		Book:    "John",
+		Chapter: 3,
+		Verse:   16,
+		Text:    "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.",
+	}
+}
+
 func main() {
-    fmt.Println("Hello, World!")
+	var verse = getVerse()
+
+	fmt.Println("Dumela ngwana waka! Tseya sebaka se go rapela.")
+	fmt.Print(verse)
 }
