05be208d159d07707341379dcb2e839ee0167158
Dinake, Keoagile <kdinake@deloitte.co.za>
2026-05-18 16:16:45 +0200

    refactor implementation to demo package operations

--- main.go
+++ main.go
@@ -2,19 +2,32 @@ package main
 
 import (
 	"fmt"
+	"math/rand/v2"
 
 	"github.com/kmdinake/rapela/rapela"
 )
 
 func main() {
-	bs := rapela.NewDefaultBibleService()
+	bs, err := rapela.NewBibleService()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Bible Service created successfully!")
+
+	fmt.Printf("Current Bible Version: %s\n", bs.GetBibleVersion())
+
+	fmt.Printf("Dumela ngwana waka! Tseya sebaka se go rapela.\n%s\n", bs.GetVerseOfTheDay())
+
 	versions := bs.GetBibleVersions()
 	fmt.Println("Available Bible Versions:")
 	for _, version := range versions {
-		fmt.Printf("- %s: %s (%s)\n", version.Id, version.Name, version.Version)
+		fmt.Println(version)
 	}
-	// var verse = bs.GetVerseOfTheDay()
 
-	// fmt.Println("Dumela ngwana waka! Tseya sebaka se go rapela.")
-	// fmt.Println(verse)
+	randomVersion := versions[rand.IntN(len(versions))]
+	fmt.Printf("Setting Bible version to: %s\n", randomVersion)
+	bs.SetBibleVersion(randomVersion)
+
+	fmt.Printf("Current Bible Version after setting: %s\n", bs.GetBibleVersion())
 }
